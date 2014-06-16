1d837b9dd170713d066c1d3bc80562efcd2400cd
nikhilisnice <nikhilisnice@gmail.com>
2014-06-15 23:10:50 -0700

    Made program that reads text file and creates files in folders with current dates names
    It made a program that will read a text file and create that
    text file in a given folder, which was named the date that the
    picture was taken. The program looks in a folder for files and
    moves it to the right folder if required.
    
    Fixes #3, and resolves #4.

--- folder-manager.py
+++ folder-manager.py
@@ -1,15 +1,26 @@
 import codecs
+import os, errno
+
+def mkdir_p(path):
+	try:
+		os.makedirs(path)
+	except OSError as exc:
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else: raise
+		
+def touch(fname, times=None):
+    with open(fname, 'a'):
+        os.utime(fname, times)
 
 f = codecs.open('sample_filenames.txt', 'r', 'utf-8')
 folders = []
-expectedOutput = [u'20130113', u'20130113', u'20130113', u'20130113', u'20130113', u'20130113', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140613', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614', u'20140614']
-
+outputfolder = '/tmp/folder-manager'
 for line in f:
   myline = line.lstrip( unicode( codecs.BOM_UTF8, "utf8" ) )
+  filename = myline
   folderdate = myline[8:16]
+  folderdatestring = str(folderdate)
   folders.append(folderdate)
-print folders
-if expectedOutput == folders:
-	print "Success!"
-else:
-	print "Try again...:("
+  mkdir_p (os.path.join(outputfolder, folderdatestring))
+  touch (os.path.join(outputfolder, folderdatestring, filename))
