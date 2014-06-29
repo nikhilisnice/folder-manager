dd83f857f96c495b5f2505725f00f45ee9fde39e
nikhilisnice <nikhilisnice@gmail.com>
2014-06-28 18:20:27 -0700

    Reads filenames in folder and moves files to selected folders
    The changes in folder-manager.py read filenames from a selected
    folder and move the files to a selected folder (the date in the
    filename). Also took out the touch function.
    
    This fixes #5.

--- folder-manager.py
+++ folder-manager.py
@@ -1,26 +1,29 @@
-import codecs
 import os, errno
+import shutil
+from os import listdir
+from os.path import isfile, join
 
 def mkdir_p(path):
-	try:
-		os.makedirs(path)
-	except OSError as exc:
-		if exc.errno == errno.EEXIST and os.path.isdir(path):
-			pass
-		else: raise
-		
-def touch(fname, times=None):
-    with open(fname, 'a'):
-        os.utime(fname, times)
+  try:
+    os.makedirs(path)
+  except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+       pass
+    else: raise
 
-f = codecs.open('sample_filenames.txt', 'r', 'utf-8')
-folders = []
+def touch(fname):
+    try:
+        os.utime(fname, None)
+    except:
+        open(fname, 'a').close()
+
+inputfolder = '/home/nikhil/Documents/Test-For-Folder-Thing'
 outputfolder = '/tmp/folder-manager'
-for line in f:
-  myline = line.lstrip( unicode( codecs.BOM_UTF8, "utf8" ) )
-  filename = myline
-  folderdate = myline[8:16]
-  folderdatestring = str(folderdate)
-  folders.append(folderdate)
-  mkdir_p (os.path.join(outputfolder, folderdatestring))
-  touch (os.path.join(outputfolder, folderdatestring, filename))
+filenamelist = os.listdir(inputfolder)
+
+for filename in filenamelist:
+  folderdate = filename[8:16]
+  filenamecheck = filename[0:3]
+  if filenamecheck == "DCS":
+	mkdir_p(os.path.join(outputfolder, folderdate))
+	shutil.move((os.path.join(inputfolder, filename)), os.path.join(outputfolder, folderdate, filename))
