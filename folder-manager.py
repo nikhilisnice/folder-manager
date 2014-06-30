f077f8208573e61e6e98d58af5c83506ab1a432b
nikhilisnice <nikhilisnice@gmail.com>
2014-06-29 17:29:30 -0700

    Made arguments so input folders could be chosen by ind. users
    Made arguments that can choose multiple input folders, and made
    a loop so that it would work for all folders. Took out some\
    hardcoding.
    
    Fixes #10.

--- folder-manager.py
+++ folder-manager.py
@@ -1,7 +1,12 @@
 import os, errno
 import shutil
+import argparse
 from os import listdir
 from os.path import isfile, join
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument("inputfolders", nargs="+", help = 'Select the input folder(s) for the program. Add the input folders&#39; paths on top ')
+args = parser.parse_args()
 
 def mkdir_p(path):
   try:
@@ -17,17 +22,25 @@ def touch(fname):
     except:
         open(fname, 'a').close()
         
-def mov_files(inputfolder, outputfolder, ds, de):
+def mov_files(inputfolder, outputfolder):
 	filenamelist = os.listdir(inputfolder)
 	for filename in filenamelist:
-	  folderdate = filename[ds:de]
 	  filenamecheck = filename[0:3]
+	  if filenamecheck == "DCS":
+		ds = 8
+		de = 16
+	  elif filenamecheck == "78A":
+	    ds = 24
+	    de = 32
+	  folderdate = filename[ds:de]
 	  if (filenamecheck == "DCS" or filenamecheck == "78A"):
 		mkdir_p(os.path.join(outputfolder, folderdate))
 		shutil.move((os.path.join(inputfolder, filename)), os.path.join(outputfolder, folderdate, filename))
-inputfolder = '/media/untitled/ftp/pi/files/files'
-inputfolder2 = '/media/untitled/ftp/pi/files/files/ipcam'
-outputfolder = '/media/untitled/ftp/pictures'
+#inputfolder = '/media/untitled/ftp/pi/files/files/ipcam'
+inputfolders= args.inputfolders
+outputfolder = '/ftp/files'
 
-mov_files(inputfolder, outputfolder, 8, 16)
-mov_files(inputfolder2, outputfolder, 24, 32)
+
+#mov_files(inputfolder, outputfolder, 8, 16)
+for folder in inputfolders:
+	mov_files(folder, outputfolder)
