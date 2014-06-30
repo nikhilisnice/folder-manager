75ad2391bb8cc5ce6cb996972535cf97173ea70f
nikhilisnice <nikhilisnice@gmail.com>
2014-06-29 18:43:42 -0700

    Made output folder an argument
    Made output folder a separate argument so that the output folder
    didn't have to be hardcoded. Also, took out some tabs and replac-
    ed with spaces (indenting).
    
    Fixes #11.

--- folder-manager.py
+++ folder-manager.py
@@ -13,34 +13,34 @@ def mkdir_p(path):
     os.makedirs(path)
   except OSError as exc:
     if exc.errno == errno.EEXIST and os.path.isdir(path):
-       pass
+      pass
     else: raise
 
 def touch(fname):
-    try:
-        os.utime(fname, None)
-    except:
-        open(fname, 'a').close()
+  try:
+    os.utime(fname, None)
+  except:
+    open(fname, 'a').close()
         
 def mov_files(inputfolder, outputfolder):
-	filenamelist = os.listdir(inputfolder)
-	for filename in filenamelist:
-	  filenamecheck = filename[0:3]
-	  if filenamecheck == "DCS":
-		ds = 8
-		de = 16
-	  elif filenamecheck == "78A":
-	    ds = 24
-	    de = 32
-	  folderdate = filename[ds:de]
-	  if (filenamecheck == "DCS" or filenamecheck == "78A"):
-		mkdir_p(os.path.join(outputfolder, folderdate))
-		shutil.move((os.path.join(inputfolder, filename)), os.path.join(outputfolder, folderdate, filename))
+  filenamelist = os.listdir(inputfolder)
+  for filename in filenamelist:
+    filenamecheck = filename[0:3]
+    if filenamecheck == "DCS":
+      ds = 8
+      de = 16
+    elif filenamecheck == "78A":
+      ds = 24
+      de = 32
+    folderdate = filename[ds:de]
+    if (filenamecheck == "DCS" or filenamecheck == "78A"):
+      mkdir_p(os.path.join(outputfolder, folderdate))
+      shutil.move((os.path.join(inputfolder, filename)), os.path.join(outputfolder, folderdate, filename))
 #inputfolder = '/media/untitled/ftp/pi/files/files/ipcam'
 inputfolders= args.inputfolders
-outputfolder = '/ftp/files'
+outputfolder = '/tmp/folder-manager'
 
 
 #mov_files(inputfolder, outputfolder, 8, 16)
 for folder in inputfolders:
-	mov_files(folder, outputfolder)
+  mov_files(folder, outputfolder)
